608597668a3c1ca9479b3c2ce6417132c30ec018
Vishwaa <vishwaa.3511@gmail.com>
2024-12-15 12:14:51 +0530

    Hono installation

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -15,16 +15,14 @@ import {
     FormItem,
     FormMessage,
 } from "@/components/ui/form";
+import { registerSchema } from "../schemas";
+import { useRegister } from "../api/use-register";
 
-const formSchema = z.object({
-    name: z.string().trim().min(1, "Required"),
-    email: z.string().email(),
-    password: z.string().min(8, "Minimum of 8 characters required"),
-})
 
 export const SignUpCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-            resolver: zodResolver(formSchema),
+    const { mutate } = useRegister();
+    const form = useForm<z.infer<typeof registerSchema>>({
+            resolver: zodResolver(registerSchema),
             defaultValues: {
                 name: "",
                 email: "",
@@ -32,8 +30,8 @@ export const SignUpCard = () => {
             },
         });
     
-        const onSubmit = (values: z.infer<typeof formSchema>) => {
-            console.log({ values });
+        const onSubmit = (values: z.infer<typeof registerSchema>) => {
+            mutate({ json: values });
         }
 
   return (
